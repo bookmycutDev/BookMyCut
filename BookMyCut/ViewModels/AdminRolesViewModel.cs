1c7c76aed61374a810b75fe82cfdb4363128c38e
Dusly Nestor <1968400@crosemont.qc.ca>
2026-03-25 00:44:32 -0400

    premiere version de connexion et gestion de roles

---
+++ BookMyCut/ViewModels/AdminRolesViewModel.cs
@@ -0,0 +1,56 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using BookMyCut.Data;
+using BookMyCut.Models;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+
+namespace BookMyCut.ViewModels
+{
+    public partial class AdminRolesViewModel : ObservableObject
+    {
+        private readonly BookMyCutContext _db = new BookMyCutContext();
+
+        // Liste update automatiquement
+        [ObservableProperty]
+        private ObservableCollection<Utilisateur> _utilisateurs = new();
+
+        //utilisateur dans tableau
+        [ObservableProperty]
+        private Utilisateur? _utilisateurSelectionne;
+
+        public AdminRolesViewModel()
+        {
+            ChargerDonnees();
+        }
+
+        //tous utilisateurs de SQLite
+        private void ChargerDonnees()
+        {
+            // utilisateurs de bookmycut.db
+            var liste = _db.Utilisateurs.ToList();
+            Utilisateurs = new ObservableCollection<Utilisateur>(liste);
+        }
+
+        [RelayCommand]
+        private void ModifierRole(RoleUtilisateur nouveauRole)
+        {
+            if (UtilisateurSelectionne == null)
+            {
+                MessageBox.Show("Veuillez sélectionner un utilisateur dans la liste.");
+                return;
+            }
+
+            // Mise à jour dans la base de données
+            UtilisateurSelectionne.Role = nouveauRole;
+            _db.Utilisateurs.Update(UtilisateurSelectionne);
+            _db.SaveChanges();
+
+            MessageBox.Show($"{UtilisateurSelectionne.NomComplet} est maintenant {nouveauRole}.");
+
+            // Rafraîchissement visuel
+            ChargerDonnees();
+        }
+    }
+}
