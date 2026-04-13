82e752b948beaafc6f0498124b43336b22223467
6176721 <6176721@OLW-S013-00-04>
2026-04-13 16:55:17 -0400

    modif

--- BookMyCut/MainWindow.xaml.cs
+++ BookMyCut/MainWindow.xaml.cs
@@ -1,7 +1,4 @@
-﻿using BookMyCut.Utils;
-using BookMyCut.ViewModels;
-using BookMyCut.Views;
-using Microsoft.Extensions.DependencyInjection;
+﻿using System.Text;
 using System.Windows;
 
 namespace BookMyCut
@@ -11,59 +8,11 @@ namespace BookMyCut
         public MainWindow()
         {
             InitializeComponent();
-            RafraichirInterface();
-            // Charger la vue d'accueil par défaut
-            ChargerAccueil();
         }
-
-        private void RafraichirInterface()
+        private void BtnInscriptionClick(object sender, RoutedEventArgs e)
         {
-            if (SessionUtilisateur.Instance.EstConnecte)
-            {
-                txtUser.Text = $"Bonjour, {SessionUtilisateur.Instance.UtilisateurConnecte.NomComplet} !";
-            }
-        }
-
-        private void ChargerAccueil()
-        {
-            // vue au ServiceProvider pour injecté ViewModel automatiquement
-            var homeView = App.ServiceProvider.GetRequiredService<HomeView>();
-
-            //force rafraîchissement 
-            if (homeView.DataContext is HomeViewModel vm)
-            {
-                // appelle méthode chargement 
-                _ = vm.ChargerRendezVousAsync();
-            }
-
-            MainContent.Content = homeView;
-        }
-
-        private void BtnDeconnexion_Click(object sender, RoutedEventArgs e)
-        {
-            SessionUtilisateur.Instance.Deconnecter();
-            var login = App.ServiceProvider.GetRequiredService<ConnexionView>();
-            login.Show();
-            this.Close();
-        }
-
-        private void BtnAccueil_Click(object sender, RoutedEventArgs e) => ChargerAccueil();
-
-        public void NaviguerVersBooking()
-        {
-            var bookingView = App.ServiceProvider.GetRequiredService<BookingView>();
-
-            if (bookingView.DataContext is BookingViewModel vm)
-            {
-                // abonne action dans le ViewModel
-                vm.SurReservationReussie = () =>
-                {
-                    // Quand action invoquée, on revient à l'accueil
-                    ChargerAccueil();
-                };
-            }
-
-            MainContent.Content = bookingView;
+            Inscription fenetre = new Inscription();
+            fenetre.Show();
         }
 
         private void BtnServices_Click(object sender, RoutedEventArgs e) => MessageBox.Show("Découvrez nos services bientôt ici ! Utilisez 'PRENDRE RENDEZ-VOUS' sur l'accueil.");
