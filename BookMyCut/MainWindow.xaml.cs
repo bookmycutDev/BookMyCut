f56b35bd126fa1352050dbee220a356f43468b5f
Riquelme04 <6176721@collegemv.qc.ca>
2026-05-09 11:28:34 -0400

    Refactor MainWindow

--- BookMyCut/MainWindow.xaml.cs
+++ BookMyCut/MainWindow.xaml.cs
@@ -1,35 +1,141 @@
-﻿using BookMyCut.Utils;
-using BookMyCut.ViewModels;
-using BookMyCut.Views;
-using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using BookMyCut.Utils;
+using BookMyCut.Views;
+using BookMyCut.ViewModels;
+using BookMyCut.Data.Models;
 
 namespace BookMyCut
 {
-    
     public partial class MainWindow : Window
     {
-        // MainViewModel injecté  grâce configuration App.xaml.cs
         public MainWindow(MainViewModel vm)
         {
             InitializeComponent();
+            MettreAJourNomUtilisateur();
+            AfficherVueInitialeSelonRole();
 
             //DataContext pour permettre les Bindings dans le XAML
             this.DataContext = vm;
         }
 
-       
+        public void MettreAJourNomUtilisateur()
+        {
+            var utilisateur = SessionUtilisateur.Instance.UtilisateurConnecte;
+
+            txtUser.Text = utilisateur != null
+                ? $"Bonjour, {utilisateur.NomComplet} !"
+                : "Bonjour !";
+        }
+
+        private void AfficherVueInitialeSelonRole()
+        {
+            var utilisateur = SessionUtilisateur.Instance.UtilisateurConnecte;
+
+            if (utilisateur != null && utilisateur.Role == RoleUtilisateur.Coiffeur)
+            {
+                AfficherDisponibilitesCoiffeur();
+            }
+            else
+            {
+                AfficherAccueil();
+            }
+        }
+
+        private void AfficherAccueil()
+        {
+            var homeView = App.ServiceProvider.GetRequiredService<HomeView>();
+            var homeVm = App.ServiceProvider.GetRequiredService<HomeViewModel>();
+
+            homeView.DataContext = homeVm;
+            MainContent.Content = homeView;
+        }
+
+        public void AfficherAccueilPublic()
+        {
+            var utilisateur = SessionUtilisateur.Instance.UtilisateurConnecte;
+
+            if (utilisateur != null && utilisateur.Role == RoleUtilisateur.Coiffeur)
+            {
+                AfficherDisponibilitesCoiffeur();
+            }
+            else
+            {
+                AfficherAccueil();
+            }
+        }
+
+        private void AfficherServices()
+        {
+            var bookingView = App.ServiceProvider.GetRequiredService<BookingView>();
+            var bookingVm = App.ServiceProvider.GetRequiredService<BookingViewModel>();
+
+            bookingVm.SurReservationReussie = () =>
+            {
+                AfficherAccueilPublic();
+            };
+
+            bookingView.DataContext = bookingVm;
+            MainContent.Content = bookingView;
+        }
+
+        private void AfficherModifierProfil()
+        {
+            var profilView = App.ServiceProvider.GetRequiredService<ModifierProfilView>();
+            MainContent.Content = profilView;
+        }
+
+        private void AfficherNosCoiffeurs()
+        {
+            var view = App.ServiceProvider.GetRequiredService<NosCoiffeursView>();
+            var vm = App.ServiceProvider.GetRequiredService<NosCoiffeursViewModel>();
+
+            view.DataContext = vm;
+            MainContent.Content = view;
+        }
+
+        private void AfficherDisponibilitesCoiffeur()
+        {
+            var view = App.ServiceProvider.GetRequiredService<DisponibilitesCoiffeurView>();
+            var vm = App.ServiceProvider.GetRequiredService<DisponibilitesCoiffeurViewModel>();
+
+            view.DataContext = vm;
+            MainContent.Content = view;
+        }
+
+        public void NaviguerVersBooking()
+        {
+            AfficherServices();
+        }
+
+        private void BtnAccueil_Click(object sender, RoutedEventArgs e)
+        {
+            AfficherAccueilPublic();
+        }
+
+        private void BtnServices_Click(object sender, RoutedEventArgs e)
+        {
+            AfficherServices();
+        }
+
+        private void BtnModifierProfil_Click(object sender, RoutedEventArgs e)
+        {
+            AfficherModifierProfil();
+        }
+
+        private void BtnNosCoiffeurs_Click(object sender, RoutedEventArgs e)
+        {
+            AfficherNosCoiffeurs();
+        }
+
         private void BtnDeconnexion_Click(object sender, RoutedEventArgs e)
         {
-            // Réinitialise la session globale
             SessionUtilisateur.Instance.Deconnecter();
 
-            // Récupère une nouvelle instance de la vue de connexion via le ServiceProvider
-            var login = App.ServiceProvider.GetRequiredService<ConnexionView>();
+            var connexionView = App.ServiceProvider.GetRequiredService<ConnexionView>();
+            connexionView.Show();
 
-            // Affiche la fenêtre de connexion et ferme la fenêtre principale
-            login.Show();
-            this.Close();
+            Close();
         }
     }
 }
